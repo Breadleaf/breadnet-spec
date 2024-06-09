d3bb01966957e7ffd8e5a7e0758f21fc0135a4f2
breadleaf <bkhnapa@gmail.com>
2024-06-08 19:40:02 -0600

    dns -- updated generate to make a more verbose .env file

--- dns/generate.py
+++ dns/generate.py
@@ -6,15 +6,28 @@ def main():
         return
 
     keys = [
-        "host",
-        "user",
-        "password",
-        "dbname",
-        "port"
+        ["Configure database information:"],
+        "db_name",
+        "db_user",
+        "db_password",
+        "db_host",
+        "db_port",
+        None,
+        ["Configure domain name server information:"],
+        "dns_host",
+        "dns_port",
     ]
 
     with open("./.env", "w") as env:
         for key in keys:
+            if key is None:
+                env.write("\n")
+                continue
+
+            if isinstance(key, list):
+                env.write(f"# {key[0]}\n")
+                continue
+
             env.write(f"{key}=\n")
 
     print("./.env file has been created with empty fields")
